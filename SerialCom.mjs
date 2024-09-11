f6ff78ce9b37bfd26c9d3652378744a99a0a87d1
11-pog <alvessamuel872@gmail.com>
2024-09-11 14:55:28 -0300

    .

--- SerialCom.mjs
+++ SerialCom.mjs
@@ -1,9 +1,39 @@
 import { SerialPort } from "serialport";
 
 class Comms {
-  constructor(port = "COM3", baudrate = 9600) {
-    this.port = port;
-    this.baud = baudrate;
-    this.Comms = new SerialPort({ path: port, baudRate: baudrate });
-  }
+    constructor(port = "COM3", baudrate = 9600) {
+        this.port = port;
+        this.baud = baudrate;
+        this.Comms = new SerialPort({ path: port, baudRate: baudrate });
+
+        this.Comms.open();
+    }
+
+    async EnsureOpen() {
+        if (!this.Comms.isOpen) {
+            throw new Error("Serial port is not open");
+        }
+    }
+
+    async Send(data) {
+        await this.EnsureOpen();
+        try {
+            this.Comms.write(data);
+            console.log(`Data sent to serial port: ${data}`);
+        } catch (err) {
+            console.error(`Error sending data to serial port: ${err}`);
+        }
+    }
+
+    async Read() {
+        await this.EnsureOpen();
+
+        try {
+            this.Comms.on("data", (data) => {
+                console.log(`Data received from serial port: ${data}`);
+            });
+        } catch (err) {
+            console.error(`Error reading data from serial port: ${err}`);
+        }
+    }
 }
