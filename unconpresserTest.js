79f443caca9ee5599bd5a2101dab08eb0e4be843
11-pog <alvessamuel872@gmail.com>
2024-09-29 21:48:33 -0300

    .

--- unconpresserTest.js
+++ unconpresserTest.js
@@ -1,10 +1,15 @@
-const idtobeuncropressed = 0;
+// Testing Site
+
+function encodeID(hours, minutes, extra)
+{
+    return (hours << 23) | (minutes << 17) | extra;
+}
 
 function decodeID(id) {
     const hours = (id >> 23) & 0x1F;       // Extrai as horas
     const minutes = (id >> 17) & 0x3F;     // Extrai os minutos
     const extra = id & 0x1FFFF;            // Extrai o valor extra
-    
+
     return {
         hours: hours,
         minutes: minutes,
@@ -13,6 +18,7 @@ function decodeID(id) {
 }
 
 
-var pedro = decodeID(idtobeuncropressed);
+var ID = encodeID(NaN, NaN, NaN)
+var pedro = decodeID(ID);
 
 console.log("cheese")
